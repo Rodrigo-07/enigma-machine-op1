f5c70e7c77f0fccf47b67755bd136227b9ae9de7
Rodrigo-07 <freirerodrigosales@gmail.com>
2025-11-28 09:54:13 -0300

    feat: enhance encipher method with rotor and plugboard state tracking

--- EnigmaMachine.js
+++ EnigmaMachine.js
@@ -18,6 +18,16 @@ class Enigma {
         this.r2 = r2;
         this.r3 = r3;
         this.pb = plugboard;
+
+        this.charR3Forward = null;
+        this.charR2Forward = null;
+        this.charR1Forward = null;
+        this.charReflect = null;
+        this.charR1Backward = null;
+        this.charR2Backward = null;
+        this.charR3Backward = null;
+        this.charPbForward = null;
+        this.charPbBackward= null;
     }
 
  
@@ -64,32 +74,41 @@ class Enigma {
     
     // Plugboard process (forward)
     if (this.pb) {
-      charPbForward = this.pb.process(charIndex);
+        charPbForward = this.pb.process(charIndex);
+        this.charPbForward = charPbForward;
     }
     console.log("Forward pb", ALPHABET[charPbForward]);
 
     // Rotores forward da direita para a esquerda
     charR3Forward = this.r3.forward(charPbForward);
+    this.charR3Forward = charR3Forward;
     console.log("R3 forward:", ALPHABET[charR3Forward]);
     charR2Forward = this.r2.forward(charR3);
+    this.charR2Forward = charR2Forward;
     console.log("R2 forward:", ALPHABET[charR2Forward]);
     charR3Forward = this.r1.forward(charR2);
+    this.charR1Forward = charR3Forward;
     console.log("R1 forward:", ALPHABET[charR3Forward]);
 
     // Reflector
     charReflect = this.ref.reflect(charR3Forward);
+    this.charReflect = charReflect;
     console.log("Reflector:", ALPHABET[charReflect]);
 
     // Rotores backward da esquerda para a direita
     charR1Backward = this.r1.backward(charReflect);
+    this.charR1Backward = charR1Backward;
     console.log("R1 backward:", ALPHABET[charR1Backward]);
     charR2Backward = this.r2.backward(charR1Backward);
+    this.charR2Backward = charR2Backward;
     console.log("R2 backward:", ALPHABET[charR2Backward]);
     charR3Backward = this.r3.backward(charR2Backward);
+    this.charR3Backward = charR3Backward;
     console.log("R3 backward:", ALPHABET[charR3Backward]);
 
     if (this.pb) {
-      charPbBackward= this.pb.process(charR3Backward);
+        charPbBackward= this.pb.process(charR3Backward);
+        this.charPbBackward = charPbBackward;
     }
     console.log("Backward pb:", ALPHABET[charPbBackward]);
 
